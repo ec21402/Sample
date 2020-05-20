2238abb6151b2dbb571365e4d6da5992da7200c7
ec21402 <ec21402@gmail.com>
2020-05-20 23:08:13 +0800

    1.Directory structure changes 2.Connect to DB and create table by enityframework core code first

---
+++ Sample/Sample.web/Domain/Product.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sample.web.Domain
+{
+    public class Product
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int BrandId { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public Category Category { get; set; }
+
+        public Brand Brand { get; set; }
+    }
+}
