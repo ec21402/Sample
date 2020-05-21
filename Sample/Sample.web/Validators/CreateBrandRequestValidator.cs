0d7db179b7912bc882bc667f81daa940516a631b
ec21402 <ec21402@gmail.com>
2020-05-21 16:48:18 +0800

    Validate brands API
    1.Add new package FluentBalidate.Core
    2.Add action filter to validate brand (UpdateBrandRequest,CreateBrandRequest)
    3.Add a errorresponse to show error

---
+++ Sample/Sample.web/Validators/CreateBrandRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Sample.web.Contracts.V1.Request;
+
+namespace Sample.web.Validators
+{
+    public class CreateBrandRequestValidator : AbstractValidator<CreateBrandRequest>
+    {
+        public CreateBrandRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .Matches("^[a-zA-Z0-9 ]*$").WithMessage("只能輸入0-9及A-Z");
+        }
+    }
+}
