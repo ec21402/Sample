5e5da2122df134a2fb4f01fe4ed270617cc3b403
ec21402 <ec21402@gmail.com>
2020-05-21 12:18:33 +0800

    Create Integration Test
    1.Create brandController test
    2.Install fluentassert to make test code readable
    3.Create memory DB for test

---
+++ Sample/Sample.IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Sample.web.Data;
+using Sample.web.Installers;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Sample.IntegrationTest
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(service =>
+                    {
+                        service.RemoveAll(typeof(DbContextOptions<DataContext>));
+                        service.AddDbContext<DataContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+
+            TestClient = appFactory.CreateClient();
+        }
+    }
+}
